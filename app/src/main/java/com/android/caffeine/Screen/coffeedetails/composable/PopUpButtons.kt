8c17c597b8de3cc814c663768e5d006321fe85b2
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-06 15:00:10 +0300

    feat: add pop up coffee button

--- app/src/main/java/com/android/caffeine/Screen/coffeedetails/composable/PopUpButtons.kt
+++ app/src/main/java/com/android/caffeine/Screen/coffeedetails/composable/PopUpButtons.kt
@@ -12,8 +12,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.android.caffeine.R
 import com.android.caffeine.ui.theme.PopUpButtonsBackground
 
 @Composable
@@ -47,6 +49,30 @@ private fun PopUpButtonsPreview() {
                 isSelected = selectedIndex == index,
                 index = index,
                 text = text,
+                onClickButton = onClick,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PopUpButtonsPreview2() {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    PopUpButtons(
+        selectedButtonIndex = selectedIndex,
+        onClickButton = { selectedIndex = it },
+    ) { selectedIndex, onClick ->
+        val buttons = listOf(
+            painterResource(R.drawable.coffee_icon_round),
+            painterResource(R.drawable.coffee_icon_round),
+            painterResource(R.drawable.coffee_icon_round)
+        )
+        buttons.forEachIndexed { index, icon ->
+            PopUpCoffeeButton(
+                isSelected = selectedIndex == index,
+                index = index,
+                icon = icon,
                 onClickButton = onClick,
             )
         }
---
+++ app/src/main/java/com/android/caffeine/Screen/coffeedetails/composable/PopUpCoffeeButton.kt
@@ -0,0 +1,62 @@
+package com.android.caffeine.Screen.coffeedetails.composable
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.unit.dp
+import com.android.caffeine.ui.theme.PopUpButtonBackground
+import com.android.caffeine.ui.theme.PopUpButtonSelectedText
+
+@Composable
+fun PopUpCoffeeButton(
+    isSelected: Boolean,
+    index: Int,
+    icon: Painter,
+    onClickButton: (Int) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .size(40.dp)
+            .clip(CircleShape)
+            .clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = { onClickButton(index) }
+            ),
+        contentAlignment = Alignment.Center
+    ){
+        AnimatedVisibility(
+            visible = isSelected,
+            enter = fadeIn(animationSpec = tween(durationMillis = 700)),
+            exit = fadeOut(animationSpec = tween(durationMillis = 700))
+        ){
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(color = PopUpButtonBackground, shape = CircleShape),
+                contentAlignment = Alignment.Center
+            ){
+                Icon(
+                    painter = icon,
+                    contentDescription = null,
+                    tint = PopUpButtonSelectedText,
+                    modifier = Modifier.size(19.5.dp)
+                )
+            }
+        }
+    }
+}
