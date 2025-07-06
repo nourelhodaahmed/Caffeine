38c4043da3a0a826ae96e37400ac81a16ef7de1c
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-06 21:18:37 +0300

    feat: add coffee details view model

--- app/src/main/java/com/android/caffeine/model/CoffeeCup.kt
+++ app/src/main/java/com/android/caffeine/model/CoffeeCup.kt
@@ -1,7 +1,5 @@
 package com.android.caffeine.model
 
-import androidx.compose.ui.graphics.painter.Painter
-
 data class CoffeeCup(
     val id: Int,
     val type: String,
--- app/src/main/java/com/android/caffeine/screen/coffeedetails/CoffeeDetailsViewModel.kt
+++ app/src/main/java/com/android/caffeine/screen/coffeedetails/CoffeeDetailsViewModel.kt
@@ -1,7 +1,25 @@
 package com.android.caffeine.screen.coffeedetails
 
 import androidx.lifecycle.ViewModel
+import com.android.caffeine.model.CaffeineSize
+import com.android.caffeine.model.CupSize
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class CoffeeDetailsViewModel() : ViewModel() {
+    private val _state = MutableStateFlow<CoffeeDetailsUiState>(CoffeeDetailsUiState())
+    val state: StateFlow<CoffeeDetailsUiState> = _state.asStateFlow()
 
+    fun onChangeCupSize(size: CupSize){
+        _state.update { it.copy(coffeeCup = it.coffeeCup.copy(cupSize = size)) }
+    }
+
+    fun onChangeCaffeineSize(size: CaffeineSize){
+        val newCaffeineSizes = _state.value.coffeeCup.caffeineSize.toMutableList()
+        if (newCaffeineSizes.contains(size)) newCaffeineSizes.remove(size)
+        else newCaffeineSizes.add(size)
+        _state.update { it.copy(coffeeCup = it.coffeeCup.copy(caffeineSize = newCaffeineSizes)) }
+    }
 }
