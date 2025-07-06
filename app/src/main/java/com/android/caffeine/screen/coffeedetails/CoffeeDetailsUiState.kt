480830de6488d9013e2afd28eaf7ebd689aa6115
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-06 21:18:24 +0300

    feat: add coffee details ui state

--- app/src/main/java/com/android/caffeine/screen/coffeedetails/CoffeeDetailsUiState.kt
+++ app/src/main/java/com/android/caffeine/screen/coffeedetails/CoffeeDetailsUiState.kt
@@ -1,5 +1,12 @@
 package com.android.caffeine.screen.coffeedetails
 
-class CoffeeDetailsUiState() {
+import com.android.caffeine.R
+import com.android.caffeine.model.CoffeeCup
 
-}
+data class CoffeeDetailsUiState(
+    val coffeeCup: CoffeeCup = CoffeeCup(
+        id = 0,
+        type = "",
+        img = R.drawable.black,
+    ),
+)
