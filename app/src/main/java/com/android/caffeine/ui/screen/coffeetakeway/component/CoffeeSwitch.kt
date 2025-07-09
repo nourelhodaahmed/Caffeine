0ea811fe933f213ad047da1b8dd8c4db94afe8d4
nourelhodaahmed <nourelhodaahmed208@gmail.com>
2025-07-09 13:06:52 +0300

    feat: add coffee switch

---
+++ app/src/main/java/com/android/caffeine/ui/screen/coffeetakeway/component/CoffeeSwitch.kt
@@ -0,0 +1,114 @@
+package com.android.caffeine.ui.screen.coffeetakeway.component
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import com.android.caffeine.ui.theme.Urbanist
+
+@Composable
+fun CoffeeSwitch(
+    selected: Boolean,
+    onToggle: () -> Unit,
+    coffeeTop: Int,
+    modifier: Modifier = Modifier,
+) {
+    val toggleBackgroundColor by updateTransition(selected)
+        .animateColor(
+            transitionSpec = { tween(300, easing = FastOutSlowInEasing) },
+        ) { if (it) Color(0xFF7C351B) else Color(0xFFFFEEE7) }
+
+    Row(
+        modifier = modifier
+            .background(toggleBackgroundColor, CircleShape)
+            .clipToBounds()
+    ) {
+
+        AnimatedContent(
+            targetState = selected,
+            transitionSpec = {
+
+                val slideIn  = slideInHorizontally(
+                    initialOffsetX = { if (targetState)  it else -it},
+                    animationSpec  = tween(300, easing = FastOutSlowInEasing)
+                )
+
+                val slideOut = slideOutHorizontally(
+                    targetOffsetX = { if (targetState)  it else -it},
+                    animationSpec = tween(300, easing = FastOutSlowInEasing)
+                )
+
+                slideIn togetherWith slideOut using SizeTransform(clip = true)
+
+            },
+        ) { isOn ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                if (isOn) {
+                    CoffeeIcon(onToggle, coffeeTop)
+                    CoffeeLabel("ON", Color(0x99FFFFFF))
+                } else {
+                    CoffeeIcon(onToggle, coffeeTop)
+                    CoffeeLabel("OFF", Color(0x99000000))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun CoffeeIcon(onToggle: () -> Unit, coffeeTop: Int) {
+    Image(
+        painter = painterResource(coffeeTop),
+        contentDescription = null,
+        modifier = Modifier
+            .clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = onToggle
+        )
+            .size(40.dp)
+    )
+}
+
+@Composable
+private fun CoffeeLabel(text: String, textColor: Color) {
+    Text(
+        text = text,
+        fontFamily = Urbanist,
+        fontWeight = FontWeight.Bold,
+        fontSize = 10.sp,
+        color = textColor,
+        lineHeight = 10.sp,
+        letterSpacing = 0.25.sp,
+        modifier = Modifier.padding(horizontal = 14.dp)
+    )
+}
